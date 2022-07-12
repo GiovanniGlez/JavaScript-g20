757e1972a70ee8989fca2525be10c5e600e8a160
Gio <giovannigonzalezdelgado25@gmail.com>
2022-07-11 22:45:19 -0500

    class 04

---
+++ js/practica04.js
@@ -0,0 +1,17 @@
+/! Tarea
+
+//?     Tomar un string del usuario
+//?     Imprimir en consola el número de caracteres que tiene esa cadena
+//?     Imprimir cuantas vocales estan presentes
+//?     Imprimir cada uno de los las palabras que hay ('hola koder') => ['hola', 'koders'}
+
+
+let string = prompt("Escribe una frase");
+let vocales = string.match(/[aeiou]/gi);
+let splitResult = string.split(' ');
+
+console.log(`Numero de caracteres: ${str.length}`);
+console.log(`Numero de vocales: ${vocales.length}`);
+console.log(`Palabras: ${splitResult}`);
+console.log(`Repeat method: ${str.repeat(2)}`);
+console.log(`ReplaceAll method: ${str.replaceAll(" ", "_")}`);
---
+++ js/practicas03.js
@@ -0,0 +1,64 @@
+//Practica 1
+/*let number1 = parseInt(prompt('Proporciona un número'));
+let number2 = parseInt(prompt('Proporciona otro número'));
+let number3 = parseInt(prompt('Proporciona el último número'));
+let max = 0;
+if((number1 >= number2) && (number1 >= number3)){
+    maxNumber = number1;
+}
+else if((number2 >= number1) && (number2 >= number3)){
+    maxNumber = number2;
+}
+else{
+    maxNumber = number3;
+}
+console.log(maxNumber, "es el numero mayor");*/
+
+
+//Practica 2
+/*let num1 = prompt('Ingresa numero 1');
+let num2 = prompt('Ingresa numero 2');
+num1  > num2 ? console.log('Numero 1 es mayor que numero 2') : console.log('Numer 2 es mayor que numero 1');*/
+
+//Practica 3
+/*let num1 = prompt('Ingresa numero 1');
+let num2 = prompt('Ingresa numero 2');
+let num3 = prompt('Ingresa numero 3');
+let result = num1 > num2 && num1 > num3 ? `El numero 1 (${num1}) es el mayor` : num2 > num1 && num2 > num3 ? `El numero 2 (${num2}) es el mayor` : num3 > num1 && num3 > num2 ? `El numero 3 (${num3}) es el mayor` : 'Los numeros son iguales'; 
+console.log(result); */
+
+//Practica 4
+/*let a = parseInt( prompt('Ingresa numero 1'));
+let b = parseInt( prompt('Ingresa numero 2'));
+a > b ? console.log("a/b=" ,a / b) : a < b ? console.log("a+b=", a + b) : console.log("a*b=", a * b);
+//console.log(result);*/
+
+//Practica 5
+/*Calculadora con switch*/
+
+/*console.log("1. Suma");
+console.log("2. Resta");
+console.log("3. Multiplicacion");
+console.log("4. Division");
+let opc = parseInt(prompt("Elige una operacion"));
+let num1 =parseInt(prompt("Ingresa numero 1"));
+let num2 =parseInt(prompt("Ingresa numero 2"));
+let result;
+switch(opc){
+    case 1:
+        result = num1 + num2;
+        break;
+    case 2:
+        result = num1 - num2;
+        break;
+    case 3:
+        result = num1 * num2;
+        break;
+    case 4:
+        result = num1 / num2;
+        break;
+    default:
+        result = "Operacion no valida";
+        break;
+}
+console.log(`El resultado de la operacion seleccionada es: ${result}`);*/
